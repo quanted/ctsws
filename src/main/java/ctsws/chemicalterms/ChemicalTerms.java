1eb1b0bcd7495597cdc32f607256f4924e3e0c28
kurtw555 <kurt.wolfe@gmail.com>
2017-09-18 14:20:09 -0400

    Intial commit

---
+++ src/main/java/ctsws/chemicalterms/ChemicalTerms.java
@@ -0,0 +1,78 @@
+package ctsws.chemicalterms;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.json.JSONObject;
+
+import chemaxon.formats.MolImporter;
+import chemaxon.jep.*;
+import chemaxon.jep.context.MolContext;
+import chemaxon.struc.Molecule;
+import ctsws.util.StatusMessaging;
+
+@Path("/chemicalterms")
+public class ChemicalTerms 
+{
+	public ChemicalTerms()
+	{
+
+	}
+	
+	//This method is called if HTML is request
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public String evaluateTerm(String chemTermParams)
+	{
+		JSONObject joResults = new JSONObject();
+		//JSONObject joProps = null;
+	    String results = null;
+		try
+		{
+			JSONObject joParams = new JSONObject(chemTermParams);
+			
+			if (joParams.has("substrate") == false)		 				
+				return StatusMessaging.errorMessage("No chemical substrate specified").toString();	
+			
+			String substrate = joParams.getString("substrate");
+			
+			if (joParams.has("chemicalterms") == false)		 				
+				return StatusMessaging.errorMessage("No chemical terms specified").toString();	
+			
+			String chemTerms = joParams.getString("chemicalterms");
+			//chemTerms = "logP()";
+			
+			Evaluator evaluator = new Evaluator();
+			
+			// create ChemJEP, compile the Chemical Terms expression
+			//ChemJEP chemJEP = evaluator.compile("logP()", MolContext.class);
+			ChemJEP chemJEP = evaluator.compile(chemTerms, MolContext.class);
+
+			// create context
+			MolContext context = new MolContext();
+			Molecule mol = MolImporter.importMol(substrate);
+			context.setMolecule(mol);
+
+		    // get the result by evaluating the expression
+		    // note: "logP()" expression returns a double, soAKIAI7TRNQM2US6MNA2AAKIAI7TRNQM2US6MNA2A
+		    // evaluate_double(ChemContext) method is used			
+			Object result = chemJEP.evaluate(context);
+		    //double result = chemJEP.evaluate_double(context);
+		    
+		    joResults.put("status", "success");
+			joResults.put("results", result.toString());
+			results = joResults.toString();
+		
+		}
+		catch(Exception ex)
+		{
+			results = StatusMessaging.errorMessage(ex.getMessage()).toString();
+		}
+		
+		return results;
+	}
+}
---
+++ src/main/java/ctsws/metabolism/Metabolizer.java
@@ -0,0 +1,354 @@
+package ctsws.metabolism;
+
+import java.util.*;
+import java.io.IOException;
+import java.util.Collections;
+
+
+import javax.servlet.ServletContext;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+
+//import com.chemaxon.calculations.io.formats.*;
+
+import chemaxon.formats.MolImporter;
+//import chemaxon.formats.MolImporter;
+import chemaxon.metabolism.*;
+import chemaxon.struc.*;
+//import chemaxon.util.*;
+//import chemaxon.formats.*;
+
+import org.json.*;
+
+
+// Plain old Java Object it does not extend as class or implements
+// an interface
+
+// The class registers its methods for the HTTP GET request using the @GET annotation.
+// Using the @Produces annotation, it defines that it can deliver several MIME types,
+// text, XML and HTML.
+
+// The browser requests per default the HTML MIME type.
+
+//Sets the path to base URL + /hello
+@Path("/metabolizer")
+public class Metabolizer {
+	
+	private ServletContext context;
+	public Metabolizer(@Context ServletContext contextIn)
+	{
+		context = contextIn;
+	}
+	//@Context ServletContext context;
+	
+	//This method is called if HTML is request
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getMetabolites(String sMetabParams)
+	{
+		JSONObject joReturn = null;
+		String retVal = null;
+
+		String realPath = context.getRealPath("/");
+
+		try
+		{
+			JSONObject metabolizerParams = new JSONObject(sMetabParams);
+
+			String structure = metabolizerParams.getString("structure");
+			JSONArray transformationLibs = null;
+			if (metabolizerParams.has("transformationLibraries"))
+				transformationLibs = metabolizerParams.getJSONArray("transformationLibraries");
+
+			double likelyLimit = metabolizerParams.getDouble("likelyLimit");
+      
+			int genLimit = metabolizerParams.getInt("generationLimit");
+		    int popLimit = metabolizerParams.getInt("populationLimit");
+		    String excludeCond = metabolizerParams.getString("excludeCondition");
+
+
+		    ArrayList<RxnMolecule> reactionList = new ArrayList<RxnMolecule>();
+		    //Get a list of reactions from reaction libraries.
+		    if (transformationLibs != null)
+		    {
+    	
+		    	String transLib = null;
+		    	MolImporter importer = null;
+    	  
+		    	//If two libraries are specified, then use the combined abiotic reduction 
+		    	//& hydrolysis library
+		    	int nLibs = transformationLibs.length();
+		    	switch(nLibs)
+		    	{
+		    		case 0:
+		    			break;
+		    		case 1:
+		    			//transLib = transformationLibs.getString(0);
+						transLib = context.getInitParameter(transformationLibs.getString(0));
+		    			break;
+		    		case 2:
+		    			transLib = context.getInitParameter("combined_abioticreduction_hydrolysis");
+		    			break;
+		    		default:
+		    			break;
+		    			//Need to throw error
+		    	}
+		    	
+		    	//for (int i=0;i<nLibs;i++)
+		    	//{
+		    	if (transLib != null)
+		    	{
+		    		//transLib = context.getInitParameter(transformationLibs.getString(i));
+		    		importer = new MolImporter(realPath + transLib);
+		    		RxnMolecule tempMolecule;
+		    		while ((tempMolecule=((RxnMolecule)importer.read()))!= null)
+		    		{
+		    			reactionList.add(tempMolecule);
+		    		}
+		    		importer.close();
+		    	}
+		    	//}
+		    }
+
+
+		    //If no reaction libraries are specified, the default (Human metabolism) library is loaded.
+		    chemaxon.metabolism.Metabolizer mtblzer = new chemaxon.metabolism.Metabolizer();
+		    if (transformationLibs != null)
+		    	mtblzer.setReactions(reactionList);
+
+		    mtblzer.setLikelyLimit(likelyLimit);
+		    mtblzer.setPopulationLimit(popLimit);
+		    mtblzer.setGenerationLimit(genLimit);
+		    mtblzer.setExcludeCondition(excludeCond);     
+      
+		    //This guy does most of the work
+		    List<Metabolite> lstMetabolites = metabolize(mtblzer, structure);
+      
+      
+		    JSONObject joMetabs = new JSONObject();
+		    JSONObject joParent = new JSONObject();      
+		    recurseMetaboliteTree(lstMetabolites.get(0), joMetabs);
+
+		    joParent.put("metabolites", joMetabs);
+		    JSONObject joMetTree = new JSONObject();
+		    joMetTree.put(structure, joParent);
+
+		    joReturn = new JSONObject();
+		    joReturn.put("results", joMetTree);
+		    joReturn.put("status", "success");
+
+		    retVal = joReturn.toString();
+
+		}
+		catch(Exception e)
+		{
+			String msg = e.getMessage();
+			retVal = "{\"status\" : \"error\", \"message\" : \"" + msg + "\"}";
+		}
+
+		return retVal;
+
+	}
+  
+  //Runs the metabolizer and returns a list of metabolites
+  private List<Metabolite> metabolize(chemaxon.metabolism.Metabolizer metabolizer, String substrate) throws Exception
+  {
+	  List<Metabolite> lstMetabolites = null;
+
+	  Molecule molecule = MolImporter.importMol(substrate);
+	  metabolizer.setSubstrate(molecule,substrate);
+
+	  // execute enumeration and get the resulting metabolite list
+	  List<Metabolite> lstMetabolitesTmp = metabolizer.enumerate();
+	  List<Metabolite> lstMetabolitesNoExtinct = new ArrayList<Metabolite>();
+
+	  for (int i=0;i<lstMetabolitesTmp.size();i++)
+	  {
+	  	Metabolite metabolite = lstMetabolitesTmp.get(i);
+	  	if (metabolite.getState() != Metabolite.State.EXTINCT)
+			lstMetabolitesNoExtinct.add(metabolite);
+	  }
+
+	  // get a list of unique metabolites sorted by global accumulation (major metabolite indicator)
+	  //lstMetabolites = metabolizer.calculateGlobals(lstMetabolitesTmp);
+	  lstMetabolites = metabolizer.calculateGlobals(lstMetabolitesNoExtinct);
+	  if (lstMetabolites != null && lstMetabolites.size() > 0)
+	  {
+		  Collections.sort(lstMetabolites, new Comparator<Metabolite>()
+		  {
+			//@Override
+			public int compare(final Metabolite o1, final Metabolite o2)
+			{
+				return o1.getGeneration() - o2.getGeneration();
+			}
+		  } );
+	  }
+
+	  return lstMetabolites;
+  }
+  
+  private void recurseMetaboliteTree(Metabolite metabolite, JSONObject joMetabIn)
+  {
+	  try
+	  {
+		  if (metabolite == null)
+			  return;
+		  
+		  JSONObject joMet = getMetaboliteProperties(metabolite);
+		  joMetabIn.put(metabolite.getKey(), joMet);
+		  
+		  JSONObject joMetChildren = new JSONObject();
+		  
+		  if (metabolite.getChildCount() > 0)
+			joMet.put("metabolites", joMetChildren);  
+		  
+		  
+		  for (int i=0;i< metabolite.getChildCount(); i++)
+		  {
+			  recurseMetaboliteTree(metabolite.getChild(i), joMetChildren);
+		  }		  
+	  }
+	  catch(Exception ex)
+	  {
+		  
+	  }	  
+  }
+  
+	      
+  private CTSMetabolite getCTSMetabolite(Metabolite metabolite) throws JSONException
+  {
+	  if (metabolite == null)
+		  return null;
+	  
+	  CTSMetabolite met = null;
+	  try
+	  {		  	 
+		  met = new CTSMetabolite(metabolite.getKey());
+		  met.accumulation =  metabolite.getAccumulation();
+	      met.degradation = metabolite.getDegradation();
+	      met.formationRate = metabolite.getFormation();
+	      met.generation = metabolite.getGeneration();
+	      met.globalAccumulation = metabolite.getGlobalAccumulation();      
+	      met.globalFormationRate = metabolite.getGlobalFormation();
+	      met.globalProduction = metabolite.getGlobalProduction();
+	      met.likelihood = metabolite.getLikelihood().toString();
+	      met.phase = metabolite.getPhase();
+	      met.production = metabolite.getProduction();
+	      met.route = metabolite.getRoute();
+	      met.routes = metabolite.getRoutes();
+	      met.state = metabolite.getState().toString();
+	      met.transmissivity = metabolite.getTransmissivity();
+	      met.parentKey = metabolite.getParent().getKey();
+      
+	  }
+	  catch(Exception ex)
+	  {
+		  
+	  }
+	  return met;
+      
+  }
+
+
+    
+  private JSONObject getMetaboliteProperties(Metabolite metabolite) throws JSONException
+  {
+    JSONObject jo = new JSONObject();
+
+    jo.put("accumulation", metabolite.getAccumulation());
+    jo.put("globalAccumulation", metabolite.getGlobalAccumulation());
+
+    jo.put("degradation", metabolite.getDegradation());
+    jo.put("generation", metabolite.getGeneration());
+
+    jo.put("formationRate", metabolite.getFormation());
+    jo.put("globalFormationRate", metabolite.getGlobalFormation());
+
+    jo.put("likelihood", metabolite.getLikelihood());
+
+    jo.put("production", metabolite.getProduction());
+    jo.put("globalProduction", metabolite.getGlobalProduction() );
+
+    jo.put("phase", metabolite.getPhase());
+    jo.put("state", metabolite.getState().toString());
+
+    jo.put("transmissivity", metabolite.getTransmissivity());
+
+    jo.put("route", metabolite.getRoute());
+    jo.put("routes", metabolite.getRoutes());
+    
+    try
+    {
+    	Metabolite metab = metabolite.getParent();
+    	if (metab != null)
+    		jo.put("parent", metab.getKey());
+
+    
+    	jo.put("smiles", metabolite.getKey());
+    }
+    catch(IOException ioEx)
+    {
+      //Not much good with out a smiles string
+      jo = null;
+    }
+
+    return jo;
+
+  }
+  
+//Recursive function for call the ChemAxon Metablolizer.
+  //Use one instance of a parameterized Metabolizer but update the substrate at each level.
+  /*
+  private List<Metabolite> metabolize2(chemaxon.metabolism.Metabolizer metabolizer, String substrate, JSONObject jo, int depth)
+  {
+    List<Metabolite> lstMetabolites = null;
+    List<Metabolite> lstMetabolitesOut = null;
+
+    //Increment our depth
+    int currentDepth = depth + 1;
+    //Get out if we have reached our generation limit
+    if (currentDepth > _genLimit)
+      return null;
+
+    try
+    {
+      Molecule molecule = MolImporter.importMol(substrate);
+      metabolizer.setSubstrate(molecule, "1");
+      lstMetabolites = metabolizer.enumerate();
+
+      int count = lstMetabolites.size();
+      for (int i=0;i<count;i++)
+      {
+        Metabolite metabolite = lstMetabolites.get(i);
+        JSONObject joMetab = getMetaboliteProperties(metabolite);
+
+        if (joMetab != null)
+        {
+          String smiles = metabolite.getKey();
+          if (smiles.equalsIgnoreCase(substrate))
+            continue;
+          //Map that contains just metabolites
+          JSONObject joMetabs = new JSONObject();
+
+          lstMetabolitesOut = metabolize2(metabolizer, metabolite.getKey(), joMetabs, currentDepth);
+          joMetab.put("metabolites", joMetabs);
+          jo.put(smiles, joMetab);
+        }
+
+      }
+
+
+    }
+    catch(Exception ex)
+    {
+
+    }
+
+    return lstMetabolitesOut;
+
+  }
+*/
+  
+
+}
